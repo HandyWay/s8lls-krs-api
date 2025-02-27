9f31aef2c86106de15017e96a6e63b52643f2e52
Robert Moń <m0ni3v@gmail.com>
2025-02-27 12:51:24 +0100

    Initial commit

---
+++ src/main/java/com/s8lls/krs_microservice/KrsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.s8lls.krs_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrsMicroserviceApplication.class, args);
+	}
+
+}
